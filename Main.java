3e3cfa98dcb8163e0127116d087bc436774cbed8
Fortune-Ndlovu <fndlovu@redhat.com>
2025-08-09 14:41:25 +0100

    java is strong but i am stronger
    Signed-off-by: Fortune-Ndlovu <fndlovu@redhat.com>

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World! Fortune");
+        String myName = "Fortune";
+        int myAge = 23;
+        System.out.println("Hello Java! My name is " + myName + " I am aged " + myAge);
+
     }
+
 }
